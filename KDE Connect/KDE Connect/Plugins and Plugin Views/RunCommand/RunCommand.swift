d24e5ca5ebb1ae446de8eeab5f2d619e46fbd996
Apollo Zhu <public-apollonian@outlook.com>
2022-05-09 06:21:41 +0000

    [Fix] Allow commandList JSON to Avoid Crashing
    Fixes crash caused by GSConnect implementation https://github.com/GSConnect/gnome-shell-extension-gsconnect/issues/1051
    
    Justification: this "bug" has already been fixed by GSConnect, which now sends a String instead of JSON. However, this fix is to ensure that older version of GSConnect (which are quite common as they're version-freezes by Long-Term-Support distribution releases) don't cause crashes with KDE Connect iOS.
    
    ## Test
    
    > Test case found in https://github.com/GSConnect/gnome-shell-extension-gsconnect/pull/1053
    
    Check that a network package body like this always works:
    
    ```json
    {
        "commandList": "{\"command-uuid\":{\"name\": \"Test Command\",\"command\":\"ls\"}}"
    }
    ```
    
    Check that a network package body like this only works with this fix:
    
    ```json
    {
        "commandList": {
            "command-uuid": {
                "name": "Test Command",
                "command": "ls"
            }
        }
    }
    ```

--- KDE Connect/KDE Connect/Plugins and Plugin Views/RunCommand/RunCommand.swift
+++ KDE Connect/KDE Connect/Plugins and Plugin Views/RunCommand/RunCommand.swift
@@ -24,16 +24,29 @@ import SwiftUI
         self.controlDevice = controlDevice
     }
     
+    typealias CommandsDictionary = [String : [String : String]]
+    
     @objc func onDevicePackageReceived(np: NetworkPackage) -> Bool {
         if (np.type == .runCommand) {
             if np.bodyHasKey("commandList") {
                 // Process the received commandList here
-                let jsonString = np.object(forKey: "commandList") as! String
-                DispatchQueue.main.async { [self] in
-                    commandEntries = processCommandsJSON(jsonString)
+                let commandsDict: CommandsDictionary
+                switch np.object(forKey: "commandList") {
+                case let jsonString as String:
+                    commandsDict = processCommandsJSON(jsonString)
+                case let dict as CommandsDictionary:
+                    print("out of date GSConnect with wrong RunCommand implementation")
+                    commandsDict = dict
+                case let somethingElse:
+                    print("unexpected commandList format \(type(of: somethingElse))")
+                    commandsDict = [:]
+                }
+                let newCommandEntries = processCommandsDict(commandsDict)
+                DispatchQueue.main.async { [weak self] in
+                    self?.commandEntries = newCommandEntries
                 }
             } else {
-                print("Runcommand packet received with no commandList, ignoring")
+                print("RunCommand packet received with no commandList, ignoring")
             }
             return true
         }
@@ -58,28 +71,32 @@ import SwiftUI
         controlDevice.send(np, tag: Int(PACKAGE_TAG_NORMAL))
     }
     
-    private func processCommandsJSON(_ json: String) -> [CommandEntry] {
+    private func processCommandsJSON(_ json: String) -> CommandsDictionary {
         guard let jsonData = json.data(using: .utf8) else {
-            return []
+            return [:]
         }
         do {
-            guard let commandsDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? [String : [String : String]] else {
-                print("RunCommand: commandList decode failed")
-                return []
+            guard let commandsDict = try JSONSerialization.jsonObject(with: jsonData, options: []) as? CommandsDictionary else {
+                print("commandList string is not convertible to CommandsDictionary")
+                return [:]
             }
-            return commandsDict.compactMap { (commandKey, commandInfo) in
-                if let commandName = commandInfo["name"],
-                    let command = commandInfo["command"] {
-                    let commandEntry = CommandEntry(name: commandName, command: command, key: commandKey)
-                    return commandEntry
-                } else {
-                    print("Command or CommandName for \(commandKey) is nil")
-                    return nil
-                }
-            }
+            return commandsDict
         } catch {
             print(error.localizedDescription)
-            return []
+            return [:]
+        }
+    }
+    
+    private func processCommandsDict(_ commandsDict: CommandsDictionary) -> [CommandEntry] {
+        return commandsDict.compactMap { (commandKey, commandInfo) in
+            if let commandName = commandInfo["name"],
+               let command = commandInfo["command"] {
+                let commandEntry = CommandEntry(name: commandName, command: command, key: commandKey)
+                return commandEntry
+            } else {
+                print("Command or CommandName for \(commandKey) is nil")
+                return nil
+            }
         }
     }
 }
