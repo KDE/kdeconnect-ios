44c739d79dc9d58acc423dad48edb87b32170001
Apollo Zhu <public-apollonian@outlook.com>
2022-05-16 03:36:59 -0700

    Done without plural support

---
+++ scripts/po2xliff.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# SPDX-FileCopyrightText: 2022 Apollo Zhu <public-apollonian@outlook.com>
+#
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
+import argparse
+import xml.etree.ElementTree as ET
+from typing import Final, Optional
+
+from babel.messages.catalog import Catalog, Message
+from babel.messages.pofile import read_po
+
+########################################
+# Constants
+########################################
+
+TOOL_VERSION: Final[int] = "0.0.1"
+"""
+Increment as semantic versioning.
+"""
+
+BUILD_NUMBER: Final[str] = "1"
+"""
+Always increase by 1 independent of TOOL_VERSION
+(i.e. do NOT reset for each new TOOL_VERSION).
+"""
+
+NO_COMMENT: Final[str] = "No comment provided by engineer."
+XLIFF_TEMPLATE: Final[str] = """\
+<?xml version="1.0" encoding="UTF-8"?>
+<xliff xmlns="urn:oasis:names:tc:xliff:document:1.2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="1.2" xsi:schemaLocation="urn:oasis:names:tc:xliff:document:1.2 http://docs.oasis-open.org/xliff/v1.2/os/xliff-core-1.2-strict.xsd">
+</xliff>
+"""
+
+########################################
+# PO to XLIFF
+########################################
+
+
+def po2xliff(catalog: Catalog) -> ET.ElementTree:
+    ET.register_namespace("", "urn:oasis:names:tc:xliff:document:1.2")
+    ET.register_namespace("xsi", "http://www.w3.org/2001/XMLSchema-instance")
+    root = ET.fromstring(XLIFF_TEMPLATE)
+
+    file: Optional[ET.Element] = None
+    body: ET.Element
+
+    locale: str = catalog.locale_identifier
+    message: Message
+    # skip the first metadata one
+    for message in catalog:
+        # "KDE Connect/..." got parsed into (KDE, None) and (Connect/..., None)
+        original = " ".join([location[0] for location in message.locations])
+        id = message.context
+        source = message.id
+        target = message.string
+        note = message.auto_comments[0] if message.auto_comments else NO_COMMENT
+
+        # must have source location
+        if not original:
+            continue
+        if file is None or file.get("original") != original:
+            file = ET.SubElement(root, "file", {
+                "original": original,
+                "source-language": "en",
+                "target-language": locale,
+                "datatype": "plaintext"
+            })
+            header = ET.SubElement(file, "header")
+            ET.SubElement(header, "tool", {
+                "tool-id": "org.kde.kdeconnect.po2xliff",
+                "tool-name": "po2xliff",
+                "tool-version": TOOL_VERSION,
+                "build-num": BUILD_NUMBER
+            })
+            body = ET.SubElement(file, "body")
+        trans_unit = ET.SubElement(body, "trans-unit", {
+            "id": id,
+            "xml:space": "preserve"
+        })
+        ET.SubElement(trans_unit, "source").text = source
+        ET.SubElement(trans_unit, "target").text = target
+        ET.SubElement(trans_unit, "note").text = note
+    return ET.ElementTree(root)
+
+########################################
+# Main
+########################################
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Convert NU gettext po files to XLIFF files'
+    )
+    parser.add_argument("-i", "--input", required=True, help="Path to po")
+    parser.add_argument("-o", "--output", required=True, help="Path to XLIFF")
+
+    args = parser.parse_args()
+    with open(args.input) as input:
+        catalog = read_po(input)
+        tree = po2xliff(catalog)
+        ET.indent(tree)
+        tree.write(args.output, encoding="UTF-8", xml_declaration=True)
--- scripts/xliff2po.py
+++ scripts/xliff2po.py
@@ -5,10 +5,12 @@
 #
 # SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
 
-from typing import AnyStr, Final, Pattern
-import xml.etree.ElementTree as ET
-import re
 import argparse
+import re
+import xml.etree.ElementTree as ET
+from typing import AnyStr, Final, Pattern
+
+from babel import Locale
 from babel.messages.catalog import Catalog
 from babel.messages.pofile import write_po
 
@@ -17,7 +19,7 @@ from babel.messages.pofile import write_po
 ########################################
 
 NS: Final[dict[str, str]] = {
-    "xliff" : "urn:oasis:names:tc:xliff:document:1.2"
+    "xliff": "urn:oasis:names:tc:xliff:document:1.2"
 }
 NO_COMMENT: Final[str] = "No comment provided by engineer."
 OBJC_FORMAT: Final[Pattern[AnyStr]] = re.compile(r"%(\d+\$)?@")
@@ -26,7 +28,8 @@ OBJC_FORMAT: Final[Pattern[AnyStr]] = re.compile(r"%(\d+\$)?@")
 # XLIFF to POT
 ########################################
 
-def xliff2po(xliff_path: str, pot: bool = True):
+
+def xliff2po(xliff_path: str, pot: bool = True) -> Catalog:
     catalog = Catalog(
         project="KDE Connect iOS",
         copyright_holder="KDE Community & contributors",
@@ -42,16 +45,21 @@ def xliff2po(xliff_path: str, pot: bool = True):
     tree = ET.parse(xliff_path)
     for file in tree.getroot().findall("xliff:file", NS):
         reference = file.get("original")
+        if not pot and not catalog.locale_identifier:
+            target_language = file.get("target-language")
+            if target_language:
+                catalog.locale = Locale(target_language)
         for trans_unit in file.find("xliff:body", NS).findall("xliff:trans-unit", NS):
             msgctxt = trans_unit.get("id")
             msgid = trans_unit.find("xliff:source", NS).text
             msgstr = trans_unit.find("xliff:target", NS).text
             extracted_comments = trans_unit.find("xliff:note", NS).text
+            no_comments = extracted_comments == NO_COMMENT
             objc_format = bool(re.search(OBJC_FORMAT, msgid))
             flags = ["objc-format"] if objc_format else ()
 
             catalog.add(
-                auto_comments=() if extracted_comments == NO_COMMENT else [extracted_comments],
+                auto_comments=() if no_comments else [extracted_comments],
                 locations=[(reference, None)],
                 flags=flags,
                 context=msgctxt,
@@ -64,8 +72,11 @@ def xliff2po(xliff_path: str, pot: bool = True):
 # Main
 ########################################
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Convert XLIFF files to GNU gettext po/pot files')
+    parser = argparse.ArgumentParser(
+        description='Convert XLIFF files to GNU gettext po/pot files'
+    )
     parser.add_argument("-P", "--pot", help="Output pot instead of po",
                         action="store_true")
     parser.add_argument("-i", "--input", required=True, help="Path to XLIFF")
