00496a40de4259a2ef192b37cd5df444824a692d
Apollo Zhu <public-apollonian@outlook.com>
2022-05-16 04:08:52 -0700

    Whitespace in file path problematic huh?

--- StaticMessages.sh
+++ StaticMessages.sh
@@ -33,6 +33,7 @@ function export_pot_file # First parameter will be the path of the pot file we h
 	# TODO: not sure how to handle plurals yet, but we don't have any stringsdict this point
 	python3 scripts/xliff2po.py --pot -i 'outdir/en.xcloc/Localized Contents/en.xliff' -o 'outdir/template.pot'
 	# FIXME: REPACKPOT expect references to have line numbers, but ... Xcode doesn't provide that
+	# FIXME: REPACKPOT expect no whitespace in file name...
 	mv 'outdir/template.pot' $potfile
 	rm -rf outdir
 }
