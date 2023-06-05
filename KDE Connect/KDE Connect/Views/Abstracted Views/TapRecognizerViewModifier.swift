ad230ba76578ec221a2c2330d1808033c8406017
Apollo Zhu <public-apollonian@outlook.com>
2023-06-05 19:23:47 +0000

    [Style] Format TapRecognizerViewModifier

--- KDE Connect/KDE Connect/Views/Abstracted Views/TapRecognizerViewModifier.swift
+++ KDE Connect/KDE Connect/Views/Abstracted Views/TapRecognizerViewModifier.swift
@@ -15,47 +15,61 @@
 import SwiftUI
 
 struct TapRecognizerViewModifier: ViewModifier {
+    @State private var singleTapIsTaped = false
     
-    @State private var singleTapIsTaped: Bool = Bool()
+    let tapSensitivity: Double
+    let singleTapAction: () -> Void
+    let doubleTapAction: () -> Void
     
-    var tapSensitivity: Double
-    var singleTapAction: () -> Void
-    var doubleTapAction: () -> Void
-    
-    init(tapSensitivity: Double, singleTapAction: @escaping () -> Void, doubleTapAction: @escaping () -> Void) {
+    init(
+        tapSensitivity: Double,
+        singleTapAction: @escaping () -> Void,
+        doubleTapAction: @escaping () -> Void
+    ) {
         self.tapSensitivity = tapSensitivity
         self.singleTapAction = singleTapAction
         self.doubleTapAction = doubleTapAction
     }
     
     func body(content: Content) -> some View {
-        
-        return content
+        content
             .gesture(simultaneouslyGesture)
-        
     }
     
-    private var singleTapGesture: some Gesture { TapGesture(count: 1).onEnded{
+    private var singleTapGesture: some Gesture {
+        TapGesture(count: 1)
+            .onEnded {
+                singleTapIsTaped = true
         
-        singleTapIsTaped = true
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + tapSensitivity) { if singleTapIsTaped { singleTapAction() } }
-        
-    } }
+                DispatchQueue.main.asyncAfter(deadline: .now() + tapSensitivity) {
+                    if singleTapIsTaped {
+                        singleTapAction()
+                    }
+                }
+            }
+    }
     
-    private var doubleTapGesture: some Gesture { TapGesture(count: 2).onEnded{ singleTapIsTaped = false; doubleTapAction() } }
+    private var doubleTapGesture: some Gesture {
+        TapGesture(count: 2)
+            .onEnded {
+                singleTapIsTaped = false
+                doubleTapAction()
+            }
+    }
     
-    private var simultaneouslyGesture: some Gesture { singleTapGesture.simultaneously(with: doubleTapGesture) }
-    
+    private var simultaneouslyGesture: some Gesture {
+        singleTapGesture.simultaneously(with: doubleTapGesture)
+    }
 }
 
-
 extension View {
-    
-    func tapRecognizer(tapSensitivity: Double, singleTapAction: @escaping () -> Void, doubleTapAction: @escaping () -> Void) -> some View {
-        
-        return self.modifier(TapRecognizerViewModifier(tapSensitivity: tapSensitivity, singleTapAction: singleTapAction, doubleTapAction: doubleTapAction))
-        
+    func tapRecognizer(
+        tapSensitivity: Double,
+        singleTapAction: @escaping () -> Void,
+        doubleTapAction: @escaping () -> Void
+    ) -> some View {
+        modifier(TapRecognizerViewModifier(tapSensitivity: tapSensitivity,
+                                           singleTapAction: singleTapAction,
+                                           doubleTapAction: doubleTapAction))
     }
-    
 }
