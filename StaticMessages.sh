534d0eb84dc28c701f71c1cbf327e4df39347fd3
Albert Astals Cid <aacid@kde.org>
2026-09-22 17:43:54 +0200

    Copy the po files back to the repo

--- StaticMessages.sh
+++ StaticMessages.sh
@@ -11,5 +11,7 @@ function export_pot_file # First parameter will be a path that will contain seve
 
 function import_po_files # First parameter will be a path that will contain several .po files with the format LANG.po
 {
-    : # noop
+    podir=$1
+    mkdir -p ./po
+    cp -r $podir ./po
 }
